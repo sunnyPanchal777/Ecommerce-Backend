53c2ba04bcf205b1fce8cb37513ace6014a29cf6
sunnyPanchal777 <panchalsunny987@gmail.com>
2023-04-30 12:50:52 +0530

    code pushed

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,4 +1,6 @@
 const {DataTypes ,sequelize} = require("sequelize")
+const validate = require("validator")
+const { default: isEmail } = require("validator/lib/isEmail")
 // const sequelize = new Sequelize('sqlite::memory:');
 
 module.exports = (sequelize, Sequelize) => {
@@ -11,7 +13,13 @@ const user = sequelize.define("users", {
     },
     email:{
         allowNull:true,
-        type:DataTypes.STRING
+        type:DataTypes.STRING,
+        validate: {
+            isEmail: {
+              args: true,
+              msg: 'Please enter a valid email address'
+            }
+          }
     },
     first_name:{
         allowNull:true,
